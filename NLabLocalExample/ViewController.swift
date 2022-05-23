2ce6f85f0c332c85c3ff96a833171f2a96a79ef4
Yasin Akbas <yasin.kbas12@gmail.com>
2022-05-23 23:18:33 +0300

    update example

--- NLabLocalExample/ViewController.swift
+++ NLabLocalExample/ViewController.swift
@@ -13,13 +13,28 @@ let client = NLClient(baseURL: URL(string: "https://reqres.in")!)
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
         getUsers().onData { user in
-            dump("--> \(user)")
+            print("--> sync data received")
+            dump("--> \(user.page)")
         }.onError { error in
-            print(error)
+            print("--> sync error received")
+            print("--> \(error)")
         }.start()
+        
+        Task {
+            async let data1 = await getUsers().onError { error in
+                print("--> async error received")
+                print(error)
+            }.startAsync()
+
+            async let data2 = await getUsers().onError({ error in
+                print("--> async error received")
+                print(error)
+            }).startAsync()
+
+            let receivedData = await [data1, data2]
+            print("-->", receivedData)
+        }
     }
 
     func getUsers() -> NLTaskDirector<User, Empty> {
@@ -28,9 +43,7 @@ class ViewController: UIViewController {
             .method(.get)
             .build().and.direct()
     }
-
 }
-
 
 struct User: Decodable {
     let page: Int
